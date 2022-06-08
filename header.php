a3965237abf5274b9493ee2c51ed7452fb87f34a
Dieter <86417485+Diizzii@users.noreply.github.com>
2022-06-08 10:38:08 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="text/html; charset=<?php bloginfo( "charset" ); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<?php wp_head(); ?>
 </head>
